104d4019b1d61201470fe840f7f85df324fa8018
SoNuKuMaRsr <sonukumarsingh1619@gmail.com>
2024-05-21 15:09:32 +0530

    Add comment, like, playlist, and tweet models

---
+++ src/models/playlist.model.js
@@ -0,0 +1,25 @@
+import mongoose, {Schema} from "mongoose";
+
+const playlistSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    videos: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Video"
+        }
+    ],
+    owner: {
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    },
+}, {timestamps})
+
+
+export const playlist = mongoose.model("Playlist", playlistSchema)
